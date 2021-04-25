0369aa25cf14138042ddd7c37576f4a8c2ce0274
Sherif Nasrat <sherif_nasrat@hotmail.com>
2021-04-25 16:31:30 +0200

    Initial Push

---
+++ Models/Chat/ChatMessage.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+public class ChatMessage
+{   
+    public string User { get; set; }
+    public string Message { get; set;}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Messenger Back-end API
+
+API for Email Sign Up, Verification, Authentication & Forgot Password using ASP.NET Core 3.1.
